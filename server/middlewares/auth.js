3844eb55f28326e41d2ac7e095ef80db73e615ba
Rohit Kadhe <rkadhe99@gmail.com>
2020-05-13 12:07:36 -0600

    Fixed bug in auth middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,14 +1,17 @@
 const authHelper = require("../helpers/authHelper");
 const Errors = require("../constants/errors");
 const MusicTransferError = require("../helpers/errorHelper").MusicTransferError;
+const HttpErrors = require("../constants/httpErrors");
 
 const { MISSING_TOKEN } = Errors;
 
 // This middleware can be used to protect any routes that require auth
 const verifyAuth = (req, res, next) => {
-  const token = req.header("Authorization");
-  if (!token) {
-    next(new MusicTransferError(MISSING_TOKEN, Errors.BAD_REQUEST));
+  const authString = req.header("Authorization");
+  const [bearer, token] = authString.split(" ");
+
+  if (bearer != "Bearer" || !token) {
+    return next(new MusicTransferError(MISSING_TOKEN, HttpErrors.BAD_REQUEST));
   }
   try {
     const decoded = authHelper.verify(token);
